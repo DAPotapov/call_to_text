c8fb32b97a5ada7dd83cbc405fe8d7cbc231f1db
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-09-30 12:44:17 +0300

    Recognizes english phrase

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 #!.venv/bin/python
 
 import logging
+import os
 import speech_recognition as sr
+import sys
+
 
 # Configure logging
 logging.basicConfig(filename=".data/log.log",
@@ -12,18 +15,35 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    recognizer = sr.Recognizer()
-    # with sr.Microphone() as source:
-    #     print("Say something:")
-    #     audio = recognizer.listen(source)
+    # Check for command-line usage
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python app.py <file to process>")
 
-    # TODO open audio file and use it as source for recognizer
+    input_file = sys.argv[1]
+    folder = os.path.dirname(sys.argv[1])
+    filename = os.path.split(sys.argv[1])[1]
+    extension = os.path.splitext(filename)[1]
+    wav_filename = input_file
+
+    # TODO get mp3, decode to wav, give this wav to sr to recognize
+    
+    recognizer = sr.Recognizer()
+
+    # open audio file and use it as source for recognizer
+    with sr.AudioFile(wav_filename) as source:
+        audio = recognizer.record(source=source, duration=20)
 
     # Recognize speech using Google Speech Recognition
-    words = recognizer.recognize_google(audio)
+    try:
+        words = recognizer.recognize_google(audio)
+    except Exception as e:
+        print(e)
+        logger.error(f"Input file: {input_file} cause this error: {e}")
+    else:
+        print(words)
 
-    print(words)
+        # TODO save to <input filename>.txt
 
-
+        
 if __name__ == '__main__':
     main()
