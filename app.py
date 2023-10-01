970ba4ef315e18d1d88ae608a3f406a42d4a7f34
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-10-01 11:17:59 +0300

    Now accept other than wav audio files

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import logging
 import os
 import speech_recognition as sr
+import subprocess as sp
 import sys
 
 
@@ -17,13 +18,25 @@ logger = logging.getLogger(__name__)
 def main():
     # Check for command-line usage
     if len(sys.argv) != 2:
-        sys.exit("Usage: python app.py <file to process>")
+        sys.exit("Usage: python app.py <audio file to process>")
 
     input_file = sys.argv[1]
-    folder = os.path.dirname(sys.argv[1])
-    filename = os.path.split(sys.argv[1])[1]
-    extension = os.path.splitext(filename)[1]
-    wav_filename = input_file
+    # abs_path = os.path.abspath(sys.argv[1])
+    folder = os.path.split(sys.argv[1])[0]
+    filename = os.path.splitext(os.path.split(sys.argv[1])[1])[0] # only file name w\o extenstion
+    extension = os.path.splitext(sys.argv[1])[1]
+
+    # Check which filetype user gave to the script, try to convert if not 'wav', overwrite existing
+    if extension != '.wav':
+        # TODO make this conversion can be used on other OS (use python modules for this)
+        wav_filename = os.path.join(folder, filename + ".wav")
+        try:
+            sp.run(f"ffmpeg -y -i {sys.argv[1]} {wav_filename}", shell=True)
+        except sp.CalledProcessError as e:
+            logger.error(f"Couldn't convert '{sys.argv[1]}' to wav with this error: {e}")
+            sys.exit(f"Couldn't convert '{sys.argv[1]}' to wav. Be sure you provided audio file.")
+    else:
+        wav_filename = sys.argv[1]
 
     # TODO get mp3, decode to wav, give this wav to sr to recognize
     
